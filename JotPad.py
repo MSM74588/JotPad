3ab0bf082a7f7861b5fbc33cd09662987606896f
MSM <msm74588@gmail.com>
2024-05-15 18:04:19 +0530

    refactored imports, to prevent namespace pollution

--- JotPad.py
+++ JotPad.py
@@ -1,31 +1,31 @@
 # JotPad - Text Editor Application using tkinter
 
-from tkinter import *
+import tkinter as tk
+from tkinter import filedialog, messagebox, font
+from tkinter.constants import HORIZONTAL, VERTICAL, RIDGE, RIGHT, BOTTOM, Y, X, BOTH, END
 from tkinter.ttk import Combobox
-from tkinter.font import *
-from tkinter.filedialog import *
-from tkinter.messagebox import *
+import ttkbootstrap as ttk
 from ctypes import windll
 import os
 
-window = Tk()
+window = tk.Tk()
 windll.shcore.SetProcessDpiAwareness(1)
 window.iconbitmap("icon.ico")
 window.geometry("600x700")
 
 
 #scroll bar
-scrollx = Scrollbar(window, orient=HORIZONTAL, activerelief=RIDGE)
-scrolly = Scrollbar(window, orient=VERTICAL, activerelief=RIDGE)
+scrollx = tk.Scrollbar(window, orient=HORIZONTAL, activerelief=RIDGE)
+scrolly = tk.Scrollbar(window, orient=VERTICAL, activerelief=RIDGE)
 scrolly.pack(side=RIGHT, fill=Y)
 scrollx.pack(side=BOTTOM, fill=X)
 
 
 #text box
-txt = Text(window, font="consolas 16", wrap="none", xscrollcommand=scrollx.set, yscrollcommand=scrolly.set)
+txt = tk.Text(window, font="consolas 16", wrap="none", xscrollcommand=scrollx.set, yscrollcommand=scrolly.set)
 txt.pack(expand=True, fill=BOTH)
 txt.focus()
-txtwrap = BooleanVar(window)
+txtwrap = tk.BooleanVar(window)
 
 txtwrap.set(False)
 
@@ -41,9 +41,6 @@ working_file = ""
 def changed(*args):
     pass
 
-
-# FIXME
-
 #closing dialog box
 def confirmExit(*args):
     textarea_val = txt.get(1.0, "end-1c")
@@ -51,7 +48,8 @@ def confirmExit(*args):
         window.destroy()
     else:
         msg = "Do you really want to close JotPad?"
-        val = askyesno("Close JotPad", msg)
+        # FIXME
+        val = messagebox.askyesno("Close JotPad", msg)
         if val:
             window.destroy()
 
@@ -62,7 +60,8 @@ def confirmOpen():
     msg = "Do you really wish to open another file? \nYou will lose all the current content, unless already saved."
     content = txt.get(1.0, "end-1c")
     if content:
-        x = askyesno("File Warning", msg)
+        # FIXME
+        x = messagebox.askyesno("File Warning", msg)
         if x:
             return True
         else:
@@ -81,7 +80,7 @@ def newfile(*args):
 
 def openfile(*args):
     try:
-        fl = askopenfilename(parent=window, title="Open file")
+        fl = filedialog.askopenfilename(parent=window, title="Open file")
         if fl and confirmOpen():
             txt.delete(0.0, END)
             with open (fl, "r") as f:
@@ -92,12 +91,12 @@ def openfile(*args):
             global exist
             exist = 1
     except Exception:
-        showerror("Open file Error", "Error encountered!")
+        messagebox.showerror("Open file Error", "Error encountered!")
 
 
 def saveAs(*args):
     try:
-        fl = asksaveasfilename(parent=window, title="Save as", defaultextension=".txt",
+        fl = filedialog.asksaveasfilename(parent=window, title="Save as", defaultextension=".txt",
                                filetypes=[("Text files", ".txt"),
                                           ("All files", ".")])
         if fl:
@@ -110,7 +109,7 @@ def saveAs(*args):
             global exist
             exist = 1
     except Exception:
-        showerror("Save as Error", "Error encountered!")
+        messagebox.showerror("Save as Error", "Error encountered!")
 
 
 def save(*args):
@@ -144,13 +143,13 @@ def paste(*args):
 
 currFname = "Consolas"
 currFsize = "16"
-boldchk = BooleanVar()
-itlchk = BooleanVar()
-ulchk = BooleanVar()
-ovschk = BooleanVar()
+boldchk = tk.BooleanVar()
+itlchk = tk.BooleanVar()
+ulchk = tk.BooleanVar()
+ovschk = tk.BooleanVar()
 
 def fontSet(*args):
-    fontBox = Toplevel(window)
+    fontBox = tk.Toplevel(window)
     fontBox.transient(window)
     fontBox.rowconfigure(6, weight=1)
     fontBox.columnconfigure(2, weight=1)
@@ -159,22 +158,22 @@ def fontSet(*args):
     fontBox.resizable(0, 0)
     fontBox.title("Font Settings")
 
-    font = Label(fontBox, text="Font:", font="constanta 12 bold").grid(row=0, column=0, padx=15, pady=5)
-    fName = StringVar(fontBox)
+    font = tk.Label(fontBox, text="Font:", font="constanta 12 bold").grid(row=0, column=0, padx=15, pady=5)
+    fName = tk.StringVar(fontBox)
     fName.set(currFname)
-    fontlist = sorted([i for i in families() if i[0].isalpha()])
+    fontlist = sorted([i for i in tk.families() if i[0].isalpha()])
     fonts = Combobox(fontBox, textvariable=fName, values=fontlist, font=("", 10), state="readonly").grid(row=0, column=1)
     
-    size = Label(fontBox, text="Size:", font="constanta 12 bold"). grid(row=1, column=0, padx=15, pady=5)
-    fSize = StringVar(fontBox)
+    size = tk.Label(fontBox, text="Size:", font="constanta 12 bold"). grid(row=1, column=0, padx=15, pady=5)
+    fSize = tk.StringVar(fontBox)
     fSize.set(currFsize)
     sizelist = [i for i in range(8, 51, 2)]
     sizes = Combobox(fontBox, textvariable=fSize, values = sizelist, font=("", 10), state="readonly").grid(row=1, column=1)
     
-    bold = Checkbutton(fontBox, text="Bold", font="constanta 12 bold", variable=boldchk).grid(row=3, column=0, padx=15)
-    itl = Checkbutton(fontBox, text="Italic", font="constanta 12 italic", variable=itlchk).grid(row=3, column=1)
-    ul = Checkbutton(fontBox, text="Underline", font="constanta 12 underline", variable=ulchk).grid(row=3, column=2)
-    ovs = Checkbutton(fontBox, text="Overstrike", font="constanta 12 overstrike", variable=ovschk).grid(row=4, column=1, padx=15)
+    bold = tk.Checkbutton(fontBox, text="Bold", font="constanta 12 bold", variable=boldchk).grid(row=3, column=0, padx=15)
+    itl = tk.Checkbutton(fontBox, text="Italic", font="constanta 12 italic", variable=itlchk).grid(row=3, column=1)
+    ul = tk.Checkbutton(fontBox, text="Underline", font="constanta 12 underline", variable=ulchk).grid(row=3, column=2)
+    ovs = tk.Checkbutton(fontBox, text="Overstrike", font="constanta 12 overstrike", variable=ovschk).grid(row=4, column=1, padx=15)
 
     def styleset():
         global boldval, itlval, ulval, ovsval
@@ -237,10 +236,10 @@ def fontSet(*args):
         fsetapply()
         
 
-    Button(fontBox, text="OK", width=10, background="lightgreen", font="constanta 10 bold", command=fsetok).grid(row=5, column=0, padx=12)
-    Button(fontBox, text="Cancel", width=10, background="red", font="constanta 10 bold", command=fsetcancel).grid(row=5, column=1)
-    Button(fontBox, text="Apply", width=10, background="lightblue", font="constanta 10 bold", command=fsetapply).grid(row=5, column=2)
-    Button(fontBox, text="Reset to Default", width=16, background="lightgrey", font="constanta 10 bold", command=freset).grid(row=6, column=1)
+    tk.Button(fontBox, text="OK", width=10, background="lightgreen", font="constanta 10 bold", command=fsetok).grid(row=5, column=0, padx=12)
+    tk.Button(fontBox, text="Cancel", width=10, background="red", font="constanta 10 bold", command=fsetcancel).grid(row=5, column=1)
+    tk.Button(fontBox, text="Apply", width=10, background="lightblue", font="constanta 10 bold", command=fsetapply).grid(row=5, column=2)
+    tk.Button(fontBox, text="Reset to Default", width=16, background="lightgrey", font="constanta 10 bold", command=freset).grid(row=6, column=1)
     fontBox.focus()
     
 
@@ -256,6 +255,7 @@ def txtwrapSet(*args):
     else:
         txt.pack_forget()
         txt.config(wrap="none")
+        # FIXME
         scrollx.pack(side=BOTTOM, fill=X)
         txt.pack(expand=True, fill=BOTH)
 
@@ -263,29 +263,29 @@ def txtwrapSet(*args):
 #HELP MENU OPTIONS
 
 def about(*args):
-    aboutbox = Toplevel(window)
+    aboutbox = tk.Toplevel(window)
     aboutbox.transient(window)
     aboutbox.title("About JotPad")
     aboutbox.geometry("250x200")
     aboutbox.resizable(0, 0)
     aboutbox.iconbitmap("icon.ico")
     msg = "JotPad \nVersion: 1.0 \n\nDeveloped by: \nAnurag Chattopadhyay"
-    Label(aboutbox, text=msg, font=("", "11"), justify="left", pady=10).pack()
+    tk.Label(aboutbox, text=msg, font=("", "11"), justify="left", pady=10).pack()
     aboutbox.focus()
 
     def okpress():
         aboutbox.destroy()
         aboutbox.update()
 
-    Button(aboutbox, text="OK", font=("", "10", "bold"), width=10, command=okpress).pack()
+    tk.Button(aboutbox, text="OK", font=("", "10", "bold"), width=10, command=okpress).pack()
     
 
 
 #menubar
-menubar = Menu(window)
+menubar = tk.Menu(window)
 
 #file menu
-file = Menu(menubar, tearoff=0)
+file = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="File", menu=file)
 file.add_command(label="New File", font=("", 10), command=newfile, accelerator="Ctrl+N")
 file.add_command(label="Open File", font=("", 10), command=openfile, accelerator="Ctrl+O")
@@ -296,7 +296,7 @@ file.add_command(label="Close", font=("", 10), command=confirmExit)
 
 
 #edit menu
-edit = Menu(menubar, tearoff=0)
+edit = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Edit", menu=edit)
 edit.add_command(label="Select All", font=("", 10), command=selectAll, accelerator="Ctrl+A")
 edit.add_command(label="Cut", font=("", 10), command=cut, accelerator="Ctrl+X")
@@ -306,14 +306,14 @@ edit.add_command(label="Paste", font=("", 10), command=paste, accelerator="Ctrl+
 
 
 #view menu
-view = Menu(menubar, tearoff=0)
+view = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="View", menu=view)
 view.add_command(label="Font Settings", font=("", 10), command=fontSet)
 view.add_separator()
 view.add_checkbutton(label="Text Wrap", variable=txtwrap, command=txtwrapSet)
 
 #help menu
-help = Menu(menubar, tearoff=0)
+help = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Help", menu=help)
 help.add_command(label="About", font=("", 10), command=about)
 
