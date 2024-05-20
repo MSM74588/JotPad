9dbe07475bc48f58035e0db7762a72f6279ce206
MSM <msm74588@gmail.com>
2024-05-20 17:38:06 +0530

    added components

---
+++ components/__init__.py
@@ -0,0 +1,5 @@
+from components.codeblock import CodeView
+from components.schemeparser import _parse_scheme
+from components.languages import language_pairs
+
+# https://youtu.be/cONc0NcKE7s?si=zcVDwgrRBHS6ewN0
